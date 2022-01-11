d951df6162b41cf0a361d48bebc43a2fbcd53cc0
Rich Baird <richbai90@protonmail.com>
2022-01-11 16:30:53 -0700

    fix swap so it actually swaps

--- src/utils/swap.ts
+++ src/utils/swap.ts
@@ -1,8 +1,8 @@
 const swap = <T>(arr1: T[], arr2: T[], predicate: (a: T, b: T) => boolean): T[] => {
-  const swapped: T[] = [];
+  const swapped: T[] = [...arr1];
   for (let i = 0; i < arr1.length; i++) {
     for (let j = 0; j < arr2.length; j++) {
-      swapped.push(predicate(arr1[i], arr2[j]) ? arr2[j] : arr1[i]);
+      swapped[i] = predicate(arr1[i], arr2[j]) ? arr2[j] : arr1[i];
     }
   }
   return swapped;
